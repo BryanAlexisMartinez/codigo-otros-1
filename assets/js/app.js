22df6b2b142492efb34155b53d54f1cf7b14339e
BryanAlexisMartinez <b.alexis.mtz.a@gmail.com>
2023-08-13 23:02:43 -0600

    Correccion del codigo, se hizo funcional

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,17 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const n = document.querySelector('.name'); //se coloca .name y se elimina el signo de $
+const b = document.querySelector('.blog');// se coloca .blog y se elimina el signo de $
+const l = document.querySelector('.location');//se elimina el signo de $
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
+async function displayUser(username) { //se pone async ya que se requiere para poder usar el await
+  n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  let data = await response.json();//recupera y analiza los datos obtenidos de la API
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  n.textContent = `${data.name}`;//se elimina el signo de $ al inicio, se colocan la `` en lugar de ''
+  b.textContent = `${data.blog}`;//se elimina el signo de $ al inicio, se colocan la `` en lugar de ''
+  l.textContent = `${data.location}`;//se elimina el signo de $ al inicio, se colocan la `` en lugar de ''
 }
 
 function handleError(err) {
@@ -18,5 +19,4 @@ function handleError(err) {
   console.log(err);
   n.textContent = `Algo salió mal: ${err}`
 }
-
 displayUser('stolinski').catch(handleError);
